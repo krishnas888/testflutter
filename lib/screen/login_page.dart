5175b7f551f1844ce4effeaa910a8cd28e65b686
krishnas <verakrishnas@gmail.com>
2022-10-08 11:17:54 +0530

    fix text

--- lib/screen/login_page.dart
+++ lib/screen/login_page.dart
@@ -13,82 +13,71 @@ class LoginPage extends StatelessWidget {
           padding: const EdgeInsets.all(16),
           child: Column(
             children: [
-              Expanded(
-                  flex: 4,
-                  child: Image.asset("assets/images/login.png",
-                      fit: BoxFit.cover)),
-              const Expanded(
-                  flex: 1,
-                  child: SizedBox(
-                    height: 24,
-                  )),
-              const Expanded(
-                  flex: 1,
-                  child: Text(
-                    "Welcome to LogIn",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold),
-                  )),
-              const Expanded(
-                  flex: 1,
-                  child: SizedBox(
-                    height: 20,
-                  )),
-              Expanded(
-                flex: 3,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        decoration: const InputDecoration(
-                            hintText: "Enter username", labelText: "Username"),
+              Image.asset("assets/images/login.png",
+                  fit: BoxFit.cover),
+              SizedBox(
+                height: 24,
+              ),
+              Text(
+                "Welcome to LogIn",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          hintText: "Enter username", labelText: "Username"),
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          hintText: "Enter password", labelText: "Password"),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      height: 50,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        borderRadius:BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.deepPurpleAccent.shade200,
+                              blurRadius: 8,
+                              spreadRadius: (1),
+                              offset: const Offset(-4,-4)
+                          ),
+                          BoxShadow(
+                              color: Colors.deepPurple.shade900,
+                              blurRadius: 2,
+                              spreadRadius: (1),
+                              offset: const Offset(4,4)
+                          ),
+                        ]
                       ),
-                      TextFormField(
-                        decoration: const InputDecoration(
-                            hintText: "Enter username", labelText: "Username"),
+                      child: TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(color: Colors.white),
+                          ),
                       ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Container(
-                        height: 50,
-                        width: 200,
-                        decoration: BoxDecoration(
-                          color: Colors.deepPurple,
-                          borderRadius:BorderRadius.circular(15),
-                          boxShadow: [
-                            BoxShadow(
-                                color: Colors.deepPurpleAccent.shade200,
-                                blurRadius: 8,
-                                spreadRadius: (1),
-                                offset: const Offset(-4,-4)
-                            ),
-                            BoxShadow(
-                                color: Colors.deepPurple.shade900,
-                                blurRadius: 2,
-                                spreadRadius: (1),
-                                offset: const Offset(4,4)
-                            ),
-                          ]
-                        ),
-                        child: TextButton(
-                            onPressed: () {},
-                            child: const Text(
-                              "Login",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ],
           ),
-        )),
+        ),),
       ),
     );
   }
